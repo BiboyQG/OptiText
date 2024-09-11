720f39a9aa0b0ada9c3a7c06bebb9da68a004b66
Banghao <banghao_chi@163.com>
2024-09-11 10:30:32 -0500

    Initial Commit

---
+++ OptiText/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OptiText
+//
+//  Created by Banghao Chi on 9/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OptiText/OptiTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  OptiTextApp.swift
+//  OptiText
+//
+//  Created by Banghao Chi on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct OptiTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
