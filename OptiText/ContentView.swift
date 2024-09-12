c915083a8bc820b2d995178ec000c4ea0778a33d
Banghao <banghao_chi@163.com>
2024-09-12 17:17:30 -0500

    :rocket: Transformed to background app

--- OptiText/ContentView.swift
+++ OptiText/ContentView.swift
@@ -6,53 +6,30 @@
 //
 
 import SwiftUI
-import AppKit
-import QuickLook
 import KeyboardShortcuts
 
-extension KeyboardShortcuts.Name {
-    static let captureScreenshot = Self("captureScreenshot")
-}
-
 struct ContentView: View {
-    @State private var screenshotURL: URL?
+    @Binding var screenshotURL: URL?
+    @Binding var isPreviewPresented: Bool
     
     var body: some View {
         VStack {
             Text("Press Shift+Option+C to capture a screenshot")
                 .padding()
             KeyboardShortcuts.Recorder(for: .captureScreenshot)
-        }
-        .padding()
-        .quickLookPreview($screenshotURL)
-        .onAppear {
-            KeyboardShortcuts.onKeyDown(for: .captureScreenshot) {
-                captureScreenshot()
+            if let url = screenshotURL {
+                Button("View Screenshot") {
+                    isPreviewPresented = true
+                    if let appDelegate = NSApp.delegate as? AppDelegate {
+                        appDelegate.openInQuickLook(url: url)
+                    }
+                }
             }
         }
-    }
-    
-    func captureScreenshot() {
-        guard let screen = NSScreen.main else { return }
-        let rect = screen.frame
-        let cgImage = CGWindowListCreateImage(rect, .optionOnScreenOnly, kCGNullWindowID, .bestResolution)
-        
-        guard let cgImage = cgImage else { return }
-        let bitmapRep = NSBitmapImageRep(cgImage: cgImage)
-        guard let pngData = bitmapRep.representation(using: .png, properties: [:]) else { return }
-        
-        let tempDir = FileManager.default.temporaryDirectory
-        let fileURL = tempDir.appendingPathComponent("screenshot.png")
-        
-        do {
-            try pngData.write(to: fileURL)
-            screenshotURL = fileURL
-        } catch {
-            print("Error saving screenshot: \(error)")
-        }
+        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(screenshotURL: .constant(nil), isPreviewPresented: .constant(false))
 }
--- OptiText/OptiTextApp.swift
+++ OptiText/OptiTextApp.swift
@@ -6,21 +6,70 @@
 //
 
 import SwiftUI
+import KeyboardShortcuts
+import Quartz
 
 @main
 struct OptiTextApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @State var screenshotURL: URL?
+    @State private var isPreviewPresented = false
     
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        MenuBarExtra("OptiText", systemImage: "text.viewfinder") {
+            ContentView(screenshotURL: $screenshotURL, isPreviewPresented: $isPreviewPresented)
         }
+        .menuBarExtraStyle(.window)
+        .windowResizability(.contentSize)
     }
 }
 
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, QLPreviewPanelDataSource, QLPreviewPanelDelegate {
+    var screenshotURL: URL?
+    
     func applicationDidFinishLaunching(_ notification: Notification) {
-        // Hide dock icon
-        NSApp.setActivationPolicy(.accessory)
+        KeyboardShortcuts.onKeyDown(for: .captureScreenshot) { [self] in
+            self.captureScreenshot()
+        }
+    }
+    
+    func openInQuickLook(url: URL) {
+        self.screenshotURL = url
+        if let panel = QLPreviewPanel.shared() {
+            panel.dataSource = self
+            panel.makeKeyAndOrderFront(nil)
+        }
+    }
+    
+    func captureScreenshot() {
+        guard let screen = NSScreen.main else { return }
+        let rect = screen.frame
+        let cgImage = CGWindowListCreateImage(rect, .optionOnScreenOnly, kCGNullWindowID, .bestResolution)
+        
+        guard let cgImage = cgImage else { return }
+        let bitmapRep = NSBitmapImageRep(cgImage: cgImage)
+        guard let pngData = bitmapRep.representation(using: .png, properties: [:]) else { return }
+        
+        let tempDir = FileManager.default.temporaryDirectory
+        let fileURL = tempDir.appendingPathComponent("screenshot.png")
+        
+        do {
+            try pngData.write(to: fileURL)
+            DispatchQueue.main.async {
+                self.openInQuickLook(url: fileURL)
+            }
+        } catch {
+            print("Error saving screenshot: \(error)")
+        }
+    }
+    
+    // MARK: - QLPreviewPanelDataSource
+    
+    func numberOfPreviewItems(in panel: QLPreviewPanel!) -> Int {
+        return screenshotURL != nil ? 1 : 0
+    }
+    
+    func previewPanel(_ panel: QLPreviewPanel!, previewItemAt index: Int) -> QLPreviewItem! {
+        return screenshotURL as QLPreviewItem?
     }
 }
